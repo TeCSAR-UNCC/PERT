a46005eeee8f6e058f551f4b9913d9f6ac65d767
Reza <mbaharan@uncc.edu>
2023-12-22 10:54:00 -0500

    Adding dVAE.

---
+++ dalle_pytorch/__init__.py
@@ -0,0 +1,7 @@
+from dalle_pytorch.dalle_pytorch import DALLE
+from dalle_pytorch.dalle_pytorch import CLIP
+from dalle_pytorch.dalle_pytorch import DiscreteVAE
+from dalle_pytorch.vae import OpenAIDiscreteVAE, VQGanVAE
+
+from pkg_resources import get_distribution
+from dalle_pytorch.version import __version__
