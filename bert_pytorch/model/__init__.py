b80cddf036f3ddaa27de6e502d196743b6788ca0
gmaldonad17 <gmaldonad17@gmail.com>
2024-01-30 21:32:11 -0500

    Delete useless files, Update View frames

--- bert_pytorch/model/__init__.py
+++
@@ -1,4 +0,0 @@
-from .bert import BERT
-from .language_model import PERT_Mask_Order
-from .posebert import PoseBERT
-from .encoder import GCNEncoder
--- view_frames.py
+++ view_frames.py
@@ -1,13 +1,11 @@
 import argparse
 from configs.config import config
+import cv2
 import torch
-from configs.config import update_config
-from bert_pytorch import BERT, PoseBERT
-from bert_pytorch import BERTTrainer
 import dataset
+from configs.config import update_config
 import numpy as np
-from utils import view_skeleton_batch
-from train_2d import load_latest_model, return_dataloaders
+from utils.get_dVAE import get_dVAE
 
 
 def parse_args():
@@ -22,41 +20,69 @@ def parse_args():
 
 def main():
     args = parse_args()
+    device = torch.device(config.device)
 
-    train_dataset = eval('dataset.' + config.DATASET.train_dataset)(
-        config, config.DATASET.train_subset, is_train=True)
-    train_dataset = eval('dataset.' + config.DATASET.test_dataset)(
-        config, config.DATASET.test_subset, is_train=False)
+    test_dataset = eval('dataset.' + config.DATASET.test_dataset)(
+        config, config.DATASET.test_subset)
     
-    # vf_idx = 1411700 
+    vf_idx =  3150 
 
-    # Define the conditions
-    # condition1 = train_dataset.meta['camera'] == "00_01"
-    # condition2 = train_dataset.meta['video'].str.contains('171204_pose5')
-    # condition3 = train_dataset.meta['id'] == 0
-    # condition4 = train_dataset.meta['frame'] == "00001719"
+    data, _ = test_dataset.__getitem__(vf_idx)
+    data = np.expand_dims(data, 0)
+    data = torch.from_numpy(data).to(device)
 
-    # # Combine the conditions and filter the dataframe
-    # filtered_rows = train_dataset.meta[condition1 & condition2 & condition3 & condition4]
-    # vf_idx = np.where(train_dataset.vf == filtered_rows.index[0])[0][0]
-    vf_idx =  3150 # Strange head motion?
+    dVAE = get_dVAE(config)
+    dVAE = dVAE.to(device)
 
-    data, gt, _, mask, meta, padding = train_dataset.__getitem__(vf_idx)
+    temp = 1.0
 
-    PERT = PoseBERT(**config.MODEL)
-    pretrain = True
+    loss, recons = dVAE(
+            data, return_loss=True, return_recons=True, temp=temp
+        )
+    
+    recons = np.array(recons[0].cpu().detach().numpy())
+    video_name = "ntu_recons.mp4"
+    frame_height, frame_width = recons.shape[1], recons.shape[2]
+    out = cv2.VideoWriter(
+        video_name, cv2.VideoWriter_fourcc(*"MP4V"), 30, (frame_width, frame_height)
+    )
 
-    trainer = BERTTrainer(PERT, pretrain, None, None)
-    trainer.model, start = load_latest_model(trainer.model)
-    mask_output, _ = trainer.model(data.unsqueeze(0), mask.unsqueeze(0), torch.Tensor([padding]))
+    for i in range(recons.shape[0]):
+        # Normalize the heatmap for display
+        normalized_heatmap = cv2.normalize(
+            recons[i], None, 0, 255, cv2.NORM_MINMAX
+        )
+        colored_heatmap = cv2.applyColorMap(
+            normalized_heatmap.astype("uint8"), cv2.COLORMAP_JET
+        )
 
-    unnorm_pred = train_dataset.unnormalize_pose(mask_output, meta).cpu().detach() / torch.Tensor([1080, 1920])
-    unnorm_pred = unnorm_pred.view(-1, *unnorm_pred.shape[-2:])
-    unnorm_gt = train_dataset.unnormalize_pose(gt, meta).cpu().detach() / torch.Tensor([1080, 1920])
-    unnorm_gt = unnorm_gt.view(-1, *unnorm_gt.shape[-2:])
+        # Write to video
+        out.write(colored_heatmap)
 
-    # view_skeleton_batch(unnorm_pred, NAME=f"_pred")
-    view_skeleton_batch([unnorm_gt, unnorm_pred], NAME=f"_allmix")
+    out.release()
+
+    data = np.array(data[0].cpu().detach().numpy())
+    video_name = "ntu_sample.mp4"
+    frame_height, frame_width = data.shape[1], data.shape[2]
+    out = cv2.VideoWriter(
+        video_name, cv2.VideoWriter_fourcc(*"MP4V"), 30, (frame_width, frame_height)
+    )
+
+    for i in range(data.shape[0]):
+        # Normalize the heatmap for display
+        normalized_heatmap = cv2.normalize(
+            data[i], None, 0, 255, cv2.NORM_MINMAX
+        )
+        colored_heatmap = cv2.applyColorMap(
+            normalized_heatmap.astype("uint8"), cv2.COLORMAP_JET
+        )
+
+        # Write to video
+        out.write(colored_heatmap)
+
+    out.release()
+
+    print()
 
 if __name__ == '__main__':
     main()
